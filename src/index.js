232b3335b1198cc2e117b87b5327eeb04707554e
PARVIZ "IBRAGIM" BEKLIEV <nightkon95@gmail.com>
2022-03-26 09:57:20 +0300

    chore: rename state variable

--- src/index.js
+++ src/index.js
@@ -3,30 +3,32 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 const UseEffect = () => {
-  const [value, setValue] = useState(0);
+  const [state, setState] = useState(0);
 
   useEffect(() => {
-    if (value === 0) {
-      setValue(10 + Math.random() * 200);
+    if (state === 0) {
+      setState(10 + Math.random() * 200);
     }
-  }, [value]);
+  }, [state]);
 
   return (
     <fieldset>
       <legend>useEffect</legend>
 
-      <p>{value}</p>
-      <button onClick={() => setValue(0)}>update state</button>
+      <p>{state}</p>
+      <button onClick={() => setState(0)}>update state</button>
 
-      <p className="bad">repaint count: 2 (try to notice value flickering)</p>
+      <p className="bad">repaint count: 2 (try to notice state flickering)</p>
       <details>
         <summary>explanation (click me)</summary>
         <ol>
-          <li className="macro">macrotask / click event / setState</li>
+          <li className="macro">macrotask / click event / setState(0)</li>
           <li className="micro">microtask / react updated state</li>
           <li className="micro">microtask / react patched DOM</li>
           <li className="bad">browser repaints</li>
-          <li className="micro">microtask / useEffect / setState (again)</li>
+          <li className="micro">
+            microtask / useEffect / setState(random number)
+          </li>
           <li className="micro">microtask / react updated state</li>
           <li className="micro">microtask / react patched DOM</li>
           <li className="bad">browser repaints</li>
@@ -37,29 +39,31 @@ const UseEffect = () => {
 };
 
 const UseLayoutEffect = () => {
-  const [value, setValue] = useState(0);
+  const [state, setState] = useState(0);
 
   useLayoutEffect(() => {
-    if (value === 0) {
-      setValue(10 + Math.random() * 200);
+    if (state === 0) {
+      setState(10 + Math.random() * 200);
     }
-  }, [value]);
+  }, [state]);
 
   return (
     <fieldset>
       <legend>useLayoutEffect</legend>
 
-      <p>{value}</p>
-      <button onClick={() => setValue(0)}>update state</button>
+      <p>{state}</p>
+      <button onClick={() => setState(0)}>update state</button>
 
       <p className="ok">repaint count: 1</p>
       <details>
         <summary>explanation (click me)</summary>
         <ol>
-          <li className="macro">macrotask / click event / setState</li>
+          <li className="macro">macrotask / click event / setState(0)</li>
           <li className="micro">microtask / react updated state</li>
           <li className="micro">microtask / react patched DOM</li>
-          <li className="micro">microtask / useLayoutEffect / setState (again)</li>
+          <li className="micro">
+            microtask / useLayoutEffect / setState(random number)
+          </li>
           <li className="micro">microtask / react updated state</li>
           <li className="micro">microtask / react patched DOM</li>
           <li className="ok">browser repaints</li>
