86d06bb45f5dcd574f10aacbfeec9929a94772a1
PARVIZ "IBRAGIM" BEKLIEV <nightkon95@gmail.com>
2022-03-26 10:18:56 +0300

    chore: conclusion and advice

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ const UseEffect = () => {
 
   return (
     <fieldset>
-      <legend>useEffect</legend>
+      <legend>
+        <code>useEffect</code>
+      </legend>
 
       <p>{state}</p>
       <button onClick={() => setState(0)}>update state</button>
@@ -29,7 +31,7 @@ const UseEffect = () => {
           <li className="micro">microtask / useEffect / setState(random)</li>
           <li className="micro">microtask / react updated state</li>
           <li className="micro">microtask / react patched DOM</li>
-          <li className="bad">browser repaints</li>
+          <li className="ok">browser repaints</li>
         </ol>
       </details>
     </fieldset>
@@ -47,7 +49,9 @@ const UseLayoutEffect = () => {
 
   return (
     <fieldset>
-      <legend>useLayoutEffect</legend>
+      <legend>
+        <code>useLayoutEffect</code>
+      </legend>
 
       <p>{state}</p>
       <button onClick={() => setState(0)}>update state</button>
@@ -74,9 +78,44 @@ const UseLayoutEffect = () => {
 function App() {
   return (
     <>
-      <h5>Click to compare browser's paint behavior</h5>
+      <h3>Click to compare browser's paint behavior</h3>
       <UseEffect />
       <UseLayoutEffect />
+
+      <fieldset>
+        <legend>Conclusion</legend>
+        <p>
+          In case of logic which triggers reflow (Layout) or repaint (Paint) -
+          use <code>useLayoutEffect</code> instead
+        </p>
+        <img
+          src="https://developers.google.com/web/fundamentals/performance/rendering/images/intro/frame-full.jpg"
+          style={{ maxWidth: "100%" }}
+          alt="Javascript > Style > Layout > Paint > Composite"
+        />
+        <p>
+          Use cases when to use <code>useLayoutEffect</code>
+        </p>
+        <ul>
+          <li>
+            if your effect changes <code>className</code>
+          </li>
+          <li>if you need to update DOM in your effect</li>
+          <li>if you need to smooth your animation</li>
+          <li>etc.</li>
+        </ul>
+
+        <p>
+          You can learn more about rendering process here:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://developers.google.com/web/fundamentals/performance/rendering"
+          >
+            developers.google.com/web/fundamentals/performance/rendering
+          </a>
+        </p>
+      </fieldset>
     </>
   );
 }
